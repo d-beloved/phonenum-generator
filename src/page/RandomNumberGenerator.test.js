cf7a8abd4a906d78f3bcbdd72b639bc290296454
d-beloved <morayodeji@gmail.com>
2019-08-23 10:43:37 +0100

    add codecov

--- src/page/RandomNumberGenerator.test.js
+++ src/page/RandomNumberGenerator.test.js
@@ -1,13 +1,69 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
 import RandomGenerator from './RandomNumberGenerator';
 
-describe('Random phone number Generator', () => {
-  it('should render correctly in "debug" mode', () => {
+configure({adapter: new Adapter()});
 
-    const component = shallow(<RandomGenerator debug />);
+test('message box', () => {})
 
-    expect(component).toMatchSnapshot();
+const setup = () => {
+  const props = {
+    onInputChange: jest.fn(),
+  }
+  return shallow(<RandomGenerator {...props} />)
+}
+
+const event = {
+  preventDefault: jest.fn(),
+  target: {
+    name: '',
+    value: ''
+  }
+};
+
+describe('RandomGenerator', () => {
+  it('should render page components successfully', () => {
+    const wrapper = setup()
+    expect(wrapper.find('div').length).toBe(7)
+    expect(wrapper.find('label').length).toBe(2)
+    expect(wrapper.find('select').length).toBe(1)
+    expect(wrapper.find('table').length).toBe(1)
+    expect(wrapper.find('Button').length).toBe(2)
+    expect(wrapper.find('InputBox').length).toBe(1)
+  })
+
+  it('should call onSelectOrderChange function', () => {
+    const wrapper = setup();
+    const action = wrapper.instance();
+    const onSelectOrderChange = jest.spyOn(wrapper.instance(), 'onSelectOrderChange');
+    action.onSelectOrderChange(event);
+    expect(onSelectOrderChange).toBeCalled();
   });
-});
+
+  it('should call onInputChange function', () => {
+    const wrapper = setup();
+    const action = wrapper.instance();
+    const onInputChange = jest.spyOn(wrapper.instance(), 'onInputChange');
+    action.onInputChange(event);
+    expect(onInputChange).toBeCalled();
+  });
+
+  it('should call handleGenerateButton function', () => {
+    const wrapper = setup();
+    const action = wrapper.instance();
+    const handleGenerateButtonClick = jest.spyOn(wrapper.instance(), 'handleGenerateButton');
+    action.handleGenerateButton(event);
+    expect(handleGenerateButtonClick).toBeCalled();
+  });
+
+  it('should call handleDownload function', () => {
+    const wrapper = setup();
+    window.URL.createObjectURL = jest.fn();
+    window.URL.revokeObjectURL = jest.fn();
+    const action = wrapper.instance();
+    const handleDownload = jest.spyOn(wrapper.instance(), 'handleDownload');
+    action.handleDownload();
+    expect(handleDownload).toBeCalled();
+  });
+})
